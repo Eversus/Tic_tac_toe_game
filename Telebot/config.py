0952bcd20576c6f3ad4d8f349f05514ae739ab1d
Alexey Grigorenko <117086712+Eversus@users.noreply.github.com>
2023-09-14 23:49:51 +0300

    Create config.py

---
+++ Telebot/config.py
@@ -0,0 +1,12 @@
+# Bot name: CryptoBot
+# Username: Eversus_crypto_bot
+# Token to access the HTTP API: 6638858330:AAGQ-0skaqInuPWtACoUvdSlol4zFxdXf-Y
+
+TOKEN = "6638858330:AAGQ-0skaqInuPWtACoUvdSlol4zFxdXf-Y"
+
+# Список обрабатываемых валют
+currency = {
+    'Рубль': 'RUB',
+    'Доллар': 'USD',
+    'Евро': 'EUR'
+}
