21d6e2f38c8f9fda6eab69f7e62381daa299aeb5
Alexey Grigorenko <117086712+Eversus@users.noreply.github.com>
2023-11-06 15:08:38 +0300

    change-filter

--- News_Portal/news/templatetags/custom_filters.py
+++ News_Portal/news/templatetags/custom_filters.py
@@ -5,14 +5,14 @@ from django import template
 register = template.Library()
 
 
-censor_list = ["редиска", "поограмист", "васисуалий"]
+censor_list = ['редиска', 'поограмист', 'васисуалий']
 
-# Регистрируем наш фильтр под именем currency, чтоб Django понимал,
+# Регистрируем наш фильтр под именем currency, чтобы Django понимал,
 # что это именно фильтр для шаблонов, а не простая функция.
 @register.filter(name = "censor")
 
 def currency(value):
-   bad_words = ['редиска', 'поограмист', 'аркадий']
+   bad_words = ['редиска', 'поограмист', 'васисуалий']
    for word in bad_words:
       value = value.replace(word, '*' * len(word))
    return value
